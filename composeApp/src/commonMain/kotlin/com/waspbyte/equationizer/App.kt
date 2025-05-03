72dae0ccd54c91c7057b642cb1ebe6d8c3528444
Eryk Warot <fleserig@waspbyte.com>
2025-05-03 20:02:01 +0200

    Add theme

--- composeApp/src/commonMain/kotlin/com/waspbyte/equationizer/App.kt
+++ composeApp/src/commonMain/kotlin/com/waspbyte/equationizer/App.kt
@@ -1,7 +1,10 @@
 package com.waspbyte.equationizer
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -9,29 +12,35 @@ import com.waspbyte.equationizer.screens.ClassicScreen
 import com.waspbyte.equationizer.screens.EquationScreen
 import com.waspbyte.equationizer.screens.HomeScreen
 import com.waspbyte.equationizer.screens.NormalScreen
+import com.waspbyte.equationizer.ui.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        val navController = rememberNavController()
-
-        NavHost(
-            navController = navController,
-            startDestination = Screens.Home.route,
+    AppTheme {
+        Surface (
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            composable(Screens.Home.route) {
-                HomeScreen(navController)
-            }
-            composable(Screens.Classic.route) {
-                ClassicScreen(navController)
-            }
-            composable(Screens.Normal.route) {
-                NormalScreen(navController)
-            }
-            composable(Screens.Equation.route) {
-                EquationScreen(navController)
+            val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = Screens.Home.route,
+            ) {
+                composable(Screens.Home.route) {
+                    HomeScreen(navController)
+                }
+                composable(Screens.Classic.route) {
+                    ClassicScreen(navController)
+                }
+                composable(Screens.Normal.route) {
+                    NormalScreen(navController)
+                }
+                composable(Screens.Equation.route) {
+                    EquationScreen(navController)
+                }
             }
         }
     }
